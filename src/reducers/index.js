843d30f726adca727e73af80df71d3b820d7d1b9
Sho Ito <i.sho.0628@gmail.com>
2019-05-02 00:20:10 +0900

    add initial reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux';
+import {
+  INIT_BOARD,
+  UPDATE_BOARD,
+  PLAYER2_TURN
+} from '../actions/types';
 
-const boardReducer = () => {
+const initialBoard = () => {
   const COLUMNS = 10;
 
   let board = Array.apply(null, Array(COLUMNS)).map(() => Array(COLUMNS).fill(-1));
@@ -19,6 +24,58 @@ const boardReducer = () => {
   return board;
 };
 
+const playerReducer = (state = { isPlayer1: true }, action) => {
+  switch (action.type) {
+    case PLAYER2_TURN:
+      return { ...state, isPlayer1: false };
+    default:
+      return state;
+  }
+};
+
+const countTurnOver = (board, player, p, q, d, e) => {
+  let i;
+
+  for (i = 1; board[p+i*d][q+i*e] === (3 - player); i++) {};
+
+  if (board[p+i*d][q+i*e] === player) {
+    return i - 1;
+  } else {
+    return 0;
+  };
+};
+
+const updateBoard = ({ board, player, p, q }) => {
+  const updatedBoard = [...board];
+  let count, d, e, i;
+  const playerNum = player ? 1 : 2;
+
+  for (d = -1; d <= 1; d++) {
+    for (e = -1; e <= 1; e++) {
+      if (d === 0 && e === 0) { continue; }
+      count = countTurnOver(updatedBoard, playerNum, p, q, d, e);
+      for (i = 1; i <= count; i++) {
+        updatedBoard[p+i*d][q+i*e] = playerNum;
+      }
+    }
+  }
+  updatedBoard[p][q] = playerNum;
+  return updatedBoard;
+};
+
+
+const boardReducer = (board = [], action) => {
+  switch (action.type) {
+    case INIT_BOARD:
+      return initialBoard();
+    case UPDATE_BOARD:
+      return updateBoard(action.payload);
+    default:
+      return initialBoard();
+  }
+}
+
 export default combineReducers({
+  player: playerReducer,
   board: boardReducer
 });
