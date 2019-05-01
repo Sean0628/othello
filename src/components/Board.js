dec441cf3b5f5d9a1b953d6fa3570b4c4a6ade1e
Sho Ito <i.sho.0628@gmail.com>
2019-05-01 15:37:19 +0900

    refactor board and grid, and make them one component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
-import Grid from './Grid';
+import { connect } from 'react-redux';
+import bind from 'classnames';
 import styles from '../styles/board.module.css';
 
 class Board extends Component {
-  render() {
-    let i;
-    const grids = [];
-    for (i = 0; i < 100; i++) {
-      grids.push(<Grid />)
-    }
+  renderGrids() {
+    return this.props.board.map((row, index1) => {
+      return row.map((grid, index2) => {
 
+        let pieceColor;
+        switch (grid) {
+          case 1:
+            pieceColor = styles.white;
+            break;
+          case 2:
+            pieceColor = styles.black;
+            break;
+          default:
+            pieceColor = null;
+        }
+        
+        const styleProps = bind(
+          styles.piece,
+          pieceColor
+        );
+
+        return (
+          <div className={styles.grid} key={`${index1} ${index2}`} >
+            <div className={styleProps}>
+            </div>
+          </div>
+        )
+      });
+    });
+  }
+
+  render() {
     return (
       <div className={styles.board}>
-        {grids}
+        {this.renderGrids()}
       </div>
     );
   }
 };
 
-export default Board;
+const mapStateToProps = state => {
+  return { board: state.board };
+}
+
+export default connect(mapStateToProps)(Board);
