2391e8deab9249d172e01ebb1717f9d30458508b
Sho Ito <i.sho.0628@gmail.com>
2019-05-01 12:38:31 +0900

    apply basic css

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import Grid from './Grid';
+import styles from '../styles/board.module.css';
 
 class Board extends Component {
   render() {
+    let i;
+    const grids = [];
+    for (i = 0; i < 100; i++) {
+      grids.push(<Grid />)
+    }
+
     return (
-      <div>Board</div>
+      <div className={styles.board}>
+        {grids}
+      </div>
     );
   }
 };
---
+++ src/components/Grid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../styles/grid.module.css';
+
+const Grid = () => {
+  return (
+    <div className={styles.grid}>
+      <div className={styles.piece}>
+      </div>
+    </div>
+  );
+}
+
+export default Grid;
