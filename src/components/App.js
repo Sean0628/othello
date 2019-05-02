b2c859f41a40f5cc38f43b3406fa2c612ba53301
Sho Ito <i.sho.0628@gmail.com>
2019-05-02 18:18:25 +0900

    add react-flash-message library, and refactor to wire it up with the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import {
+  positions,
+  Provider as AlertProvider
+} from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 import Board from './Board';
 import Header from './Header';
 import Alert from './Alert';
 
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+};
+
 const App = () => {
   return (
-    <div className='container'>
-      <Header />
-      <Alert />
-      <Board />
-    </div>
+    <AlertProvider
+      template={AlertTemplate}
+    {...options}
+    >
+      <div className='container'>
+        <Header />
+        <Alert />
+        <Board />
+      </div>
+    </AlertProvider>
   );
 };
 
--- src/components/Board.js
+++ src/components/Board.js
@@ -5,6 +5,42 @@ import styles from '../styles/board.module.css';
 import { flipPiece } from '../actions';
 
 class Board extends Component {
+  countTurnOver = (board, player, p, q, d, e) => {
+    let i;
+
+    for (i = 1; board[p+i*d][q+i*e] === (3 - player); i++) {};
+
+    if (board[p+i*d][q+i*e] === player) {
+      return i - 1;
+    } else {
+      return 0;
+    };
+  };
+
+  isValidMove = ({ board, player, p, q }) => {
+    const playerNum = player ? 1 : 2;
+    if (p < 1 || p > 8 || q < 1 || q > 8) return 0;
+    if (this.countTurnOver(board, playerNum, p, q, -1,  0)) return 1;
+    if (this.countTurnOver(board, playerNum, p, q,  1,  0)) return 1;
+    if (this.countTurnOver(board, playerNum, p, q,  0, -1)) return 1;
+    if (this.countTurnOver(board, playerNum, p, q,  0,  1)) return 1;
+    if (this.countTurnOver(board, playerNum, p, q, -1, -1)) return 1;
+    if (this.countTurnOver(board, playerNum, p, q, -1,  1)) return 1;
+    if (this.countTurnOver(board, playerNum, p, q,  1, -1)) return 1;
+    if (this.countTurnOver(board, playerNum, p, q,  1,  1)) return 1;
+    return 0;
+  };
+
+  displayIndex = (grid, index1, index2) => {
+    if (grid === -1) {
+      if (index1 === 0) {
+        return index2;
+      } else if (index2 === 0) {
+        return index1;
+      }
+    }
+  }
+
   renderGrids() {
     return this.props.board.map((row, index1) => {
       return row.map((grid, index2) => {
@@ -37,19 +73,13 @@ class Board extends Component {
 
         const onClickHangling = () => {
           if (grid === 0) {
-            return this.props.flipPiece(params);
+            if (this.isValidMove(params) === 1) {
+              return this.props.flipPiece(params);
+            } else {
+              return console.log('this is not');
+            }
           } else {
             return false;
-          }
-        }
-
-        const displayIndex = () => {
-          if (grid === -1) {
-            if (index1 === 0) {
-              return index2;
-            } else if (index2 === 0) {
-              return index1;
-            }
           }
         }
 
@@ -70,7 +100,7 @@ class Board extends Component {
             key={`[${index1}][${index2}]`}
           >
             <div className={pieceStyleProps}>
-              {displayIndex()}
+              {this.displayIndex(grid, index1, index2)}
             </div>
           </div>
         )
